71ae9663234590dc04740932665b57ab4f1083e1
Telle Miller <millerincome@gmail.com>
2022-11-13 16:36:25 -0600

    Initial commit

---
+++ mergefiles.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+directory = filedialog.askdirectory()
+images = []
+
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        images.append(Image.open(directory + '/' + filename))
+    else:
+        continue
+
+for image in images:
+    images[images.index(image)] = image.convert('RGB')
+
+if len(images) > 1:
+    images[0].save(directory + '/mergedImages.pdf', save_all=True, append_images=images[1:])
+else:
+    images[0].save(directory + '/mergedImages.pdf', save_all=True)
+
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        os.remove(directory + '/' + filename)
